d48022bc400ec79bee403dd0f0d97b38b85f0a7b
02874712 <70228981+02874712@users.noreply.github.com>
2020-11-03 15:53:51 -0500

    Checks for invalid input (i.e. NULL)

--- project2.s
+++ project2.s
@@ -1,7 +1,18 @@
-.data
-output: .asciiz "\nOutput: "
+.input:  .space 1000
+output: .asciiz "\nOutput: "		#output
+invalid: .asciiz " Invalid Input"	#invalid input
 .text
 main:
+beqz input, Outputi		
+
+Output:
 li $v0 ,  4
 la $a0 ,  output
 syscall
+
+Outputi:
+li $v0 ,  4
+la $a0 ,  output
+syscall
+li $v0, 4 
+la $a0,  invalid
