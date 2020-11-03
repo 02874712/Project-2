6e112d270808dfe633e9c38b9027907d37f10551
02874712 <70228981+02874712@users.noreply.github.com>
2020-11-03 16:01:13 -0500

    Update on Invalid input

--- project2.s
+++ project2.s
@@ -1,18 +1,27 @@
-.input:  .space 1000
+.data
+input:  .space 1000
 output: .asciiz "\nOutput: "		#output
 invalid: .asciiz " Invalid Input"	#invalid input
 .text
 main:
-beqz input, Outputi		
+la $a1, input		#stores address of the input
+lb $t0, 0($a1)		#reads first byte of the input
+beqz $t0, Outputi	#compares if $t0 == 0, j Outputi 
 
-Output:
+Output:           #prints output
 li $v0 ,  4
 la $a0 ,  output
 syscall
 
-Outputi:
+Outputi:          #if input invalid
 li $v0 ,  4
 la $a0 ,  output
 syscall
 li $v0, 4 
 la $a0,  invalid
+syscall
+j End
+
+End: 
+li $v0, 10
+syscall
